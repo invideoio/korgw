4ded665d65c1d562592341191aead29fa9bb7d73
Lokesh Sharma <lokeshsharma@Lokeshs-MacBook-Pro.local>
2020-12-21 14:36:09 +0530

    modifying main renderbuffer

--- korgw/src/commonMain/kotlin/com/soywiz/korag/AG.kt
+++ korgw/src/commonMain/kotlin/com/soywiz/korag/AG.kt
@@ -33,6 +33,11 @@ interface AGContainer {
 	fun repaint(): Unit
 }
 
+enum class AGTarget {
+    DISPLAY,
+    OFFSCREEN
+}
+
 interface AGWindow : AGContainer {
 }
 
@@ -617,7 +622,14 @@ abstract class AG : Extra by Extra.Mixin() {
         }
     }
 
-	val mainRenderBuffer: BaseRenderBuffer by lazy { createMainRenderBuffer() }
+    var agTarget = AGTarget.DISPLAY
+
+	val mainRenderBuffer: BaseRenderBuffer by lazy {
+        when (agTarget) {
+            AGTarget.DISPLAY -> createMainRenderBuffer()
+            AGTarget.OFFSCREEN -> createRenderBuffer()
+        }
+    }
 
     open fun createMainRenderBuffer() = BaseRenderBufferImpl()
 
